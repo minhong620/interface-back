72581fae14061628547534b04663b06f16c5ce8c
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-05-21 16:07:58 +0900

    refactor(swagger): 테스트서버, 배포서버 값 명시

--- src/main/java/com/example/ifclubserver/common/config/SwaggerConfig.java
+++ src/main/java/com/example/ifclubserver/common/config/SwaggerConfig.java
@@ -1,38 +1,41 @@
 package com.example.ifclubserver.common.config;
 
-;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @OpenAPIDefinition(
-        info = @Info(title = "Interface Club Management",
-                description = "인터페이스 소모임 관리 시스템",
-                version = "0.1v"))
+    info = @Info(title = "Interface Club Management",
+        description = "인터페이스 소모임 관리 시스템",
+        version = "0.1v"))
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         SecurityScheme apiKey = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization")
-                .scheme("bearer")
-                .bearerFormat("JWT");
+            .type(SecurityScheme.Type.HTTP)
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization")
+            .scheme("bearer")
+            .bearerFormat("JWT");
 
         SecurityRequirement securityRequirement = new SecurityRequirement()
-                .addList("Bearer Token");
+            .addList("Bearer Token");
+
+        Server server1 = new Server().url("http://220.85.169.165:8084").description("Generated server url");
+        Server server2 = new Server().url("http://localhost:8080").description("Local server");
 
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
-                .addSecurityItem(securityRequirement);
+            .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+            .addSecurityItem(securityRequirement)
+            .servers(Arrays.asList(server1, server2));
     }
 }
-
-
