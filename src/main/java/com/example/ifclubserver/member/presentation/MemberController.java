16717f588f49b90f42d7ead1da2c32d95e33d450
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-05-23 19:19:18 +0900

    feat(config): CORS 에러수정 : 헤더추가

--- src/main/java/com/example/ifclubserver/member/presentation/MemberController.java
+++ src/main/java/com/example/ifclubserver/member/presentation/MemberController.java
@@ -6,7 +6,9 @@ import com.example.ifclubserver.member.domain.dto.request.CreateMemberRequest;
 import com.example.ifclubserver.member.domain.dto.request.MemberUpdateRequest;
 import com.example.ifclubserver.member.domain.dto.response.CreateMemberResponse;
 import com.example.ifclubserver.member.domain.entity.Member;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +20,12 @@ public class MemberController {
 
 
     @PostMapping("/members")
-    public ResponseEntity<CreateMemberResponse> create(CreateMemberRequest form) {
+    public ResponseEntity<CreateMemberResponse> create(CreateMemberRequest form, HttpServletResponse httpServletResponse) {
+        // CORS 허용 헤더 추가
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://220.85.169.165:3032");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, Authorization");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
 
         // 멤버 저장 로직 호출
         CreateMemberResponse createMemberResponse = memberServiceImpl.createMember(form);
@@ -28,15 +35,25 @@ public class MemberController {
 
     @GetMapping("/members/{id}")
     public ResponseEntity<MemberDto> getMember(
-            @PathVariable("id") Long id
+            @PathVariable("id") Long id , HttpServletResponse httpServletResponse
     ) {
+        // CORS 허용 헤더 추가
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://220.85.169.165:3032");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, Authorization");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
         MemberDto memberDto = memberServiceImpl.getMember(id);
 
         return ResponseEntity.ok(memberDto);
     }
 
     @DeleteMapping("/members/{id}")
-    public ResponseEntity<Void> deleteMember(@PathVariable long id) {
+    public ResponseEntity<Void> deleteMember(@PathVariable long id, HttpServletResponse httpServletResponse) {
+        // CORS 허용 헤더 추가
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://220.85.169.165:3032");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, Authorization");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
         memberServiceImpl.deleteMember(id);
 
         return ResponseEntity.ok()
@@ -44,7 +61,12 @@ public class MemberController {
     }
 
     @PutMapping("/members/{id}")
-    public ResponseEntity<MemberDto> updateMember(@PathVariable long id, @RequestBody MemberUpdateRequest memberUpdate) {
+    public ResponseEntity<MemberDto> updateMember(@PathVariable long id, @RequestBody MemberUpdateRequest memberUpdate, HttpServletResponse httpServletResponse) {
+        // CORS 허용 헤더 추가
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://220.85.169.165:3032");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, Authorization");
+        httpServletResponse.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
 
         return ResponseEntity.ok()
                 .body(memberServiceImpl.updateMember(id, memberUpdate));
