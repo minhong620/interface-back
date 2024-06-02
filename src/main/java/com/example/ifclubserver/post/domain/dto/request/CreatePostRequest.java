bd3e51e9503c03bb6a1a09fcb45ec741527c50c3
eugene <81062608+eugene225@users.noreply.github.com>
2024-06-02 18:42:50 +0900

    feat : postController 기본 메서드 구현

--- src/main/java/com/example/ifclubserver/post/domain/dto/request/CreatePostRequest.java
+++ src/main/java/com/example/ifclubserver/post/domain/dto/request/CreatePostRequest.java
@@ -1,7 +1,5 @@
 package com.example.ifclubserver.post.domain.dto.request;
 
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
 public record CreatePostRequest(
--- src/main/java/com/example/ifclubserver/post/presentation/PostController.java
+++ src/main/java/com/example/ifclubserver/post/presentation/PostController.java
@@ -2,9 +2,15 @@ package com.example.ifclubserver.post.presentation;
 
 import com.example.ifclubserver.common.docs.PostControllerDocs;
 import com.example.ifclubserver.post.application.PostService;
+import com.example.ifclubserver.post.domain.dto.request.CreatePostRequest;
+import com.example.ifclubserver.post.domain.dto.request.UpdatePostRequest;
+import com.example.ifclubserver.post.domain.dto.response.CreatePostResponse;
+import com.example.ifclubserver.post.domain.dto.response.PostDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -12,5 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController implements PostControllerDocs {
 
     private final PostService postService;
+
+    @PostMapping("/")
+    ResponseEntity<CreatePostResponse> create(@RequestBody CreatePostRequest request) {
+        CreatePostResponse createPostResponse = postService.createPost(request);
+        return ResponseEntity.ok(createPostResponse);
+    }
+
+    @GetMapping("/")
+    ResponseEntity<List<PostDto>> getPosts(@RequestParam Long clubId) {
+        List<PostDto> postDtos = postService.getPosts(clubId);
+        return ResponseEntity.ok(postDtos);
+    }
+
+    @GetMapping("/{postId}")
+    ResponseEntity<PostDto> getPost(@PathVariable Long postId) {
+        PostDto postDto = postService.getPost(postId);
+        return ResponseEntity.ok(postDto);
+    }
+
+    @PatchMapping("/{postId}")
+    ResponseEntity<PostDto> updatePost(@PathVariable Long postId, @RequestBody UpdatePostRequest request) {
+        PostDto postDto = postService.updatePost(postId, request);
+        return ResponseEntity.ok(postDto);
+    }
+
+    @DeleteMapping("/{postId}")
+    ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+        postService.deletePost(postId);
+        return ResponseEntity.ok().build();
+    }
 
 }
