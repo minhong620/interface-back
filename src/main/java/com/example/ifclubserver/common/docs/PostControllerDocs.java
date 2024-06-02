bf09877ef278af9c9533163954fd65a90306ed5a
eugene <81062608+eugene225@users.noreply.github.com>
2024-06-02 18:16:11 +0900

    docs : post controller docs 인터페이스 구성

---
+++ src/main/java/com/example/ifclubserver/common/docs/PostControllerDocs.java
@@ -0,0 +1,7 @@
+package com.example.ifclubserver.common.docs;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "게시글 API", description = "게시글 관련 API")
+public interface PostControllerDocs {
+}
