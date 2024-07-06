8c950a5b43045d9dd665559f429ef691972c9042
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-07 00:38:45 +0900

    feat : 로그인, 회원가입 시 필요한 에러타입 추가

--- src/main/java/com/example/ifclubserver/member/exception/MemberErrorType.java
+++ src/main/java/com/example/ifclubserver/member/exception/MemberErrorType.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum MemberErrorType {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않습니다."),
-    EMAIL_ALREADY_EXIST(HttpStatus.CONFLICT, "이메일이 이미 존재합니다.")
+    EMAIL_ALREADY_EXIST(HttpStatus.CONFLICT, "이메일이 이미 존재합니다."),
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "이메일이 존재하지 않습니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.")
     ;
 
     private final HttpStatus status;
