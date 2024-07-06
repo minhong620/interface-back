c462ad7614eadbdc5e33498995d194cfa2fbfd7d
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-06 23:46:26 +0900

    refactor : member 회원가입 서비스 로직 리팩토링

--- src/main/java/com/example/ifclubserver/member/application/impl/MemberServiceImpl.java
+++ src/main/java/com/example/ifclubserver/member/application/impl/MemberServiceImpl.java
@@ -3,9 +3,14 @@ package com.example.ifclubserver.member.application.impl;
 import com.example.ifclubserver.member.application.MemberService;
 import com.example.ifclubserver.member.domain.dto.MemberDto;
 import com.example.ifclubserver.member.domain.dto.request.CreateMemberRequest;
+import com.example.ifclubserver.member.domain.dto.request.LoginMemberRequest;
+import com.example.ifclubserver.member.domain.dto.request.SignUpMemberRequest;
 import com.example.ifclubserver.member.domain.dto.request.UpdateMemberRequest;
 import com.example.ifclubserver.member.domain.dto.response.CreateMemberResponse;
+import com.example.ifclubserver.member.domain.dto.response.LoginMemberResponse;
 import com.example.ifclubserver.member.domain.entity.Member;
+import com.example.ifclubserver.member.domain.entity.constants.MemberRole;
+import com.example.ifclubserver.member.domain.entity.constants.MemberStatus;
 import com.example.ifclubserver.member.domain.repository.MemberRepository;
 import com.example.ifclubserver.member.exception.MemberErrorType;
 import com.example.ifclubserver.member.exception.MemberException;
@@ -34,7 +39,7 @@ public class MemberServiceImpl implements MemberService {
     public MemberDto getMember(Long id) {
         // DB에서 Member 가져오기
         Member member = memberRepository.findById(id)
-            .orElseThrow(() -> new MemberException(MemberErrorType.MEMBER_NOT_FOUND));
+                .orElseThrow(() -> new MemberException(MemberErrorType.MEMBER_NOT_FOUND));
 
         // Optional인 Member를 Member로 바꾸는거
         return MemberDto.from(member);
@@ -44,8 +49,8 @@ public class MemberServiceImpl implements MemberService {
     public List<MemberDto> getMembers() {
         List<Member> members = memberRepository.findAll();
         return members.stream()
-            .map(MemberDto::from)
-            .collect(Collectors.toList());
+                .map(MemberDto::from)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -57,7 +62,7 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public MemberDto updateMember(Long id, UpdateMemberRequest request) {
         Member member = memberRepository.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
         member.updateName(request.name());
         member.updateStudentId(request.studentId());
         member.updatePhone(request.phone());
@@ -66,36 +71,30 @@ public class MemberServiceImpl implements MemberService {
         return MemberDto.from(memberRepository.save(member));
     }
 
-    public MemberDto save(MemberSignUpForm form) {
+    public MemberDto save(SignUpMemberRequest form) {
         // 이메일 중복 확인
-        if (memberRepository.findByEmail(form.getEmail()).isPresent()) {
-            throw new MemberException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        if (memberRepository.findByEmail(form.email()).isPresent()) {
+            throw new MemberException(MemberErrorType.EMAIL_ALREADY_EXIST);
         }
 
         return MemberDto.from(
-            memberRepository.save(Member.builder()
-                .email(form.getEmail())
-                .password(passwordEncoder.encode(form.getPassword()))
-                .characterName(form.getCharacterName())
-                .role(MemberRole.USER)
-                .status(MemberStatus.ACTIVE)
-                .profileImageUrl(null)
-                .totalScore(0) // 초기 호감도는 0으로 설정
-                .birthDate(form.getBirthDate())
-                .gender(form.getGender())
-                .mbti(form.getMbti())
-                .totalRegressionCount(0) // 초기 회귀 횟수는 0으로 설정
-                .gameProgress("NOT_STARTED") // 초기 게임 진행 상태는 'NOT_STARTED'로 설정
-                .refreshToken(null)
-                .build()
-            )
+                memberRepository.save(Member.builder()
+                        .name(form.name())
+                        .studentId(form.studentId())
+                        .password(form.password())
+                        .phone(form.phone())
+                        .email(form.email())
+                        .status(MemberStatus.ACTIVE)
+                        .role(MemberRole.USER)
+                        .build()
+                )
         );
     }
 
-    public MemberLoginResponse login(MemberLoginForm form) {
+    public LoginMemberResponse login(LoginMemberRequest form) {
         // 아이디 검증
-        Member member = memberRepository.findByEmail(form.getEmail())
-            .orElseThrow(() -> new MemberException(ErrorCode.EMAIL_NOT_FOUND));
+        Member member = memberRepository.findByEmail(form.email())
+                .orElseThrow(() -> new MemberException(ErrorCode.EMAIL_NOT_FOUND));
 
         // 비밀번호 검증
         if (!passwordEncoder.matches(form.getPassword(), member.getPassword())) {
@@ -103,9 +102,9 @@ public class MemberServiceImpl implements MemberService {
         }
 
         String accessToken = jwtUtil.createAccessToken(
-            CustomUserDetails.builder()
-                .member(member)
-                .build());
+                CustomUserDetails.builder()
+                        .member(member)
+                        .build());
         String refreshToken = tokenService.createRefreshToken(member);
 
         return MemberLoginResponse.from(member, accessToken, refreshToken);
