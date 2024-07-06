4fd360196f3f1bae0a478306819accc46a92d7fc
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-07 00:56:07 +0900

    docs : 코드 정리

--- src/main/java/com/example/ifclubserver/member/domain/dto/request/SignUpMemberRequest.java
+++ src/main/java/com/example/ifclubserver/member/domain/dto/request/SignUpMemberRequest.java
@@ -1,8 +1,10 @@
 package com.example.ifclubserver.member.domain.dto.request;
 
-import java.time.LocalDate;
-
 public record SignUpMemberRequest(
-    String name, String studentId, String password, String phone, String email
+        String name,
+        String studentId,
+        String password,
+        String phone,
+        String email
 ) {
 }
