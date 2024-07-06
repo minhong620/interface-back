dd797cae17619f424e85593707da7bfd5782a60f
eugene <81062608+eugene225@users.noreply.github.com>
2024-07-07 00:37:31 +0900

    refactor : config 빈등록 추가

--- src/main/java/com/example/ifclubserver/common/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/example/ifclubserver/common/filter/TokenAuthenticationFilter.java
@@ -1,18 +1,24 @@
 package com.example.ifclubserver.common.filter;
 
+import com.example.ifclubserver.common.jwt.JwtUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
+@RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
-  @Override
-  protected void doFilterInternal(HttpServletRequest request,
-      HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    private final JwtUtils jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
 
-  }
+    }
 }
